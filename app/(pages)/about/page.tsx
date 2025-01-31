0a20973731adc80334b2b4cb19b4893494628857
PBEEX <profbamiroengineeringexpo@gmail.com>
2025-01-31 12:25:48 +0100

    Update page.tsx

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -34,7 +34,7 @@ const About = () => {
               />
               <div className='text-darkgray leading-loose'>
                 Prof. Olufemi Adebisi Bamiro, born on September 16, 1947, is an Emeritus Professor of Mechanical Engineering at the University of Ibadan. A former Vice Chancellor and current Dean of the Faculty of Technology, he graduated with First Class Honours in 1971 and earned his PhD from McGill University in 1975. As the first professor in the university&#39;s Mechanical Engineering department, he has made significant contributions to academia and society. <br/>
-                Prof. Bamiro has received prestigious awards, including the Nigerian Society of Engineers Merit Award, UN-Habitat Best Practice Award, and fellowships with the Nigerian Society of Engineers and Nigerian Academy of Engineering. Renowned for his impactful work, he is also a dedicated family man.
+                Prof. Bamiro has received prestigious awards, including the Nigerian Society of Engineers Merit Award, UN-Habitat Best Practice Award, and fellowships with the Nigerian Society of Engineers and Nigerian Academy of Engineering. Renowned for his impactful work, he is also a dedicated family man. <br/>
                 The Prof. O. A. Bamiro Engineering Expo &#40;PBEEX&#41; is a groundbreaking initiative that brings together engineering and technology students from tertiary institutions across Nigeria. This annual event is designed to foster innovation, research, and the development of ambitious engineering projects that address real-world challenges, inspire creativity, and drive technological advancements.
               </div>
             </div>
